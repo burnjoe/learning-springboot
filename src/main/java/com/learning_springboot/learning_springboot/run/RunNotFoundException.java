3115e23089d6c5a4b76cf0a02f68eec9fdae8608
burnjoe <sabanajholo@gmail.com>
2024-07-23 18:31:08 +0800

    add custom Run exception

---
+++ src/main/java/com/learning_springboot/learning_springboot/run/RunNotFoundException.java
@@ -0,0 +1,11 @@
+package com.learning_springboot.learning_springboot.run;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RunNotFoundException extends RuntimeException {
+    public RunNotFoundException() {
+        super("Run not found");
+    }
+}
