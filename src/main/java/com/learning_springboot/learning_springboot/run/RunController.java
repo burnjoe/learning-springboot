d3f158add724a63d42165941abbdd36660b6ccd9
burnjoe <sabanajholo@gmail.com>
2024-07-23 13:21:56 +0800

    add sample GET one and all endpoints

--- src/main/java/com/learning_springboot/learning_springboot/run/RunController.java
+++ src/main/java/com/learning_springboot/learning_springboot/run/RunController.java
@@ -1,14 +1,33 @@
 package com.learning_springboot.learning_springboot.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+// This annotation allows to prepend the endpoint or uri in the specific mapping annotation
+@RequestMapping("/api/runs")
 public class RunController {
-    // A GET request specifying the endpoint for this home() method
-    @GetMapping("/hello")
-    public String home() {
-        return "Hello runners";
+
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
     }
+
+    // This will then use the prepended uri "/api/runs"
+    @GetMapping("")
+    List<Run> findAll() {
+        return runRepository.findAll();
+    }
+
+    // @PathVariable extracts the id from the placeholder {id} in URI
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        return runRepository.findById(id);
+    }
+
 }
---
+++ src/main/java/com/learning_springboot/learning_springboot/run/RunRepository.java
@@ -0,0 +1,50 @@
+package com.learning_springboot.learning_springboot.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    // Get all records
+    List<Run> findAll() {
+        return runs;
+    }
+
+    // Get one record given id
+    Run findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst()
+                .get();
+    }
+
+    // @PostConstruct allows initialization of data
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(
+           1,
+           "Morning Run",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusHours(1),
+            3,
+                Location.INDOOR
+        ));
+        runs.add(new Run(
+           2,
+           "Night Run",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusHours(1),
+            1,
+                Location.OUTDOOR
+        ));
+    }
+
+}
